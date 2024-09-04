73f96b33ff0d7510749945d466c9bc1ddde99932
xrafatest <xrafa794@gmail.com>
2024-09-03 19:06:17 -0500

    Initial commit

---
+++ js/menu.js
@@ -0,0 +1,4 @@
+    // JavaScript para alternar el menú en pantallas pequeñas
+    document.querySelector('.menu-toggle').addEventListener('click', function() {
+        document.querySelector('.bnm-2').classList.toggle('active');
+    });
---
+++ js/slider.js
@@ -0,0 +1,26 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+setInterval(() => {
+    nextSlide();
+}, 10000); // Cambia de imagen cada 5 segundos
